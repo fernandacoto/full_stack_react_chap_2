c90b11642849f0ea1c8da1f9b6a105bc656817b3
Fernanda Coto <fermarioro@gmail.com>
2019-03-30 18:46:55 -0600

    Time tracking app - Chapter 3

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,19 @@
 class TimersDashboard extends React.Component {
 	state = {
-		timers: [
-			{
-				title: 'Practice squat',
-				project: 'Gym chores',
-				id: uuid.v4(),
-				elapsed: 5456099,
-				runningSince: Date.now(),
-			},
-			{
-				title: 'Bake squash',
-				project: 'Kitchen chores',
-				id: uuid.v4(),
-				elapsed: 1273998,
-				runningSince: null,
-			},
-		]
+		timers: [],
 	}
+
+	componentDidMount(){
+		this.loadTimersFromServer();
+		setInterval(this.loadTimersFromServer, 5000);
+	}
+
+	loadTimersFromServer = () => {
+		client.getTimers((serverTimers) => (
+			this.setState({ timers: serverTimers })
+		));
+	};
+
 	handleCreateFormSubmit = (timer) => {
 		this.createTimer(timer);
 	};
@@ -37,6 +34,7 @@ class TimersDashboard extends React.Component {
 		this.setState({
 			timers: this.state.timers.concat(t),
 		});
+		client.createTimer(t);
 	};
 	updateTimer = (attrs) => {
 		this.setState({
@@ -51,15 +49,16 @@ class TimersDashboard extends React.Component {
 				}
 			}),
 		});
+		client.updateTimer(attrs);
 	};
 	deleteTimer = (timerId) => {
 		this.setState({
       timers: this.state.timers.filter(t => t.id !== timerId),
     });
 
-    // client.deleteTimer(
-    //   { id: timerId }
-    // );
+    client.deleteTimer(
+      { id: timerId }
+    );
 	};
 	startTimer = (timerId) => {
 		const now = Date.now();
@@ -74,7 +73,8 @@ class TimersDashboard extends React.Component {
 					return timer;
 				}
 			}),
-		})
+		});
+		client.startTimer({id: timerId, start: now });
 	};
 	stopTimer = (timerId) => {
 		const now = Date.now();
@@ -90,8 +90,10 @@ class TimersDashboard extends React.Component {
 				} else {
 					return timer;
 				}
-			}),
-		})
+			})
+		});
+		client.stopTimer({id: timerId, stop: now });
+
 	};
 	render() {
 		return(
